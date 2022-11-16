f1a3e6767ac7ec7fd80a26c7708c95851b5a5f33
MaercioMamedes <maerciomamedes@hotmail.com>
2022-11-16 17:59:00 -0300

    user update

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ from views.user.login import *
 from views.user.authenticate import *
 from views.user.logout import *
 from views.user.userRegister import *
+from views.user.userUpdate import *
 
 """from views.index import index
 from views.login import login
---
+++ views/user/userUpdate.py
@@ -0,0 +1,37 @@
+from main import app, db
+from models.users import *
+from flask import render_template, session, redirect, url_for, flash, request
+
+
+@app.route('/atualizar-cadastro', methods=['GET','POST'])
+def user_update():
+    if request.method == 'POST':
+        user = Users.query.filter_by(nickname=session['user_logged']).first()
+        new_name = request.form['name']
+        new_nickname = request.form['user']
+        password = request.form['password']
+
+        if user.auth(password):
+            user.name = new_name
+            user.nickname = new_nickname
+
+            db.session.add(user)
+            db.session.commit()
+            session['user_logged'] = user.nickname
+
+            flash('Dados alterados com sucesso', 'success')
+            return redirect(url_for('index'))
+
+        else:
+            flash('Senha inválida')
+            return render_template('updateUser.html')
+
+    else:
+        if 'user_logged' in session and session['user_logged'] is not None:
+            nickname = session['user_logged']
+            user = Users.query.filter_by(nickname=nickname).first()
+
+            return render_template('updateUser.html', data_user=user)
+        else:
+            flash('requisição inválida','danger')
+            return redirect(url_for('index'))
